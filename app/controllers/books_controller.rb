9990ad229af78ad73a699c57562bddf2ea89dac3
yuri0427a <yuri0427ita@gmail.com>
2020-01-20 11:39:02 +0000

    [Fix]コントローラアクション変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,18 @@
 class BooksController < ApplicationController
 
-  def new
-   @book = Book.new
+  def top
   end
+
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
    def create
-         book = Book.new(list_params)
+         book = Book.new(book_params)
          book.save
         # トップ画面へリダイレクト
-        redirect_to ''
+        redirect_to books_path
     end
 
     def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   get 'books/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :books
- post 'books' =>'books#create'
+  resources :books
 
-  get 'books/:id' => 'books#show', as: 'book'
+  root :to => 'books#top'
+
+  post 'books' =>'books#create'
+
+  post 'books/:id' => 'books#show'
+
+
+
 end
