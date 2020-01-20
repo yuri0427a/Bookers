481c8f61e41f1cb1555ee5e94438eb87de34ae1c
yuri0427a <yuri0427ita@gmail.com>
2020-01-20 17:49:50 +0900

    [Add]投稿機能再度追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,21 +3,15 @@ class BooksController < ApplicationController
   def new
    @book = Book.new
   end
-
-  def create
-  	book = Book.new(book_params)
-  	book.save
-  	redirect_to '/show'
-  	# 後で/newを書き換える
-  end
-
-  def show
-    @book = Book.find(params[:id]) 
-  end
-
-  private
-
-  def book_params
-  	params.require(:book).permit(:title, :body)
-  end
+   def create
+         book = Book.new(list_params)
+         book.save
+        # トップ画面へリダイレクト
+        redirect_to ''
+    end
+    private
+   
+    def book_params
+        params.require(:book).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'todolists/new'
+  get 'books/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :books
-  get 'show' => 'books#show'#後で
-
-  post 'books' => 'books#create'
-
-  get 'books/:id' => 'books#show', as: 'book'
+ post 'books' =>'books#create'
 end
