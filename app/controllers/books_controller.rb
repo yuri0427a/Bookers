d06c842d7ff965952243186e5c6d396be3776a15
yuri0427a <yuri0427ita@gmail.com>
2020-01-22 11:46:42 +0000

    [Modified]各Viewページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,46 +4,43 @@ class BooksController < ApplicationController
   end
 
   def index
-  	@books = Book.all
-  	@book = Book.new
+  	  @books = Book.all
+  	  @book = Book.new
   end
 
-    def create
-         @book = Book.new(book_params)
-         if @book.save
-          redirect_to  book_path(@book.id), notice: "Book was successfully created."
-         else
-          @books = Book.all
-          render :index
-         end
+  def create
+      @book = Book.new(book_params)
+   if @book.save
+      redirect_to  book_path(@book.id), notice: "Book was successfully created."
+   else
+      @books = Book.all
+      render :index
+   end
+  end
 
-    end         
+  def show
+      @book = Book.find(params[:id])
+  end
 
-    def show
-        @book = Book.find(params[:id])
-    end
+  def edit
+      @book = Book.find(params[:id])
+  end
 
-    def edit
-        @book = Book.find(params[:id])
-    end
-
-    def update
-        @book = Book.find(params[:id])
-        if @book.update(book_params)
-        redirect_to book_path(@book.id), notice: "Book was successfully updated."
-        else
-          @books = Book.all
-          render :edit
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book.id), notice: "Book was successfully updated."
+    else
+       @books = Book.all
+       render :edit
         end
     end
-
-
-    def  destroy
-      books= Book.find(params[:id])
-      if books.destroy
-      redirect_to books_path, notice: "Book was successfully destroyed."
+  def  destroy
+       books= Book.find(params[:id])
+    if books.destroy
+       redirect_to books_path, notice: "Book was successfully destroyed."
     end
-    end
+  end
 
     private
 
