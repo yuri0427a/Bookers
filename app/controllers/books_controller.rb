698104efafc2cb2238bcce10487a7b69bff508b8
yuri0427a <yuri0427ita@gmail.com>
2020-01-22 07:50:41 +0000

    [Finish]投稿、アップデート、削除、サクセスメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,9 +10,10 @@ class BooksController < ApplicationController
 
     def create
          book = Book.new(book_params)
-         book.save
-        redirect_to  book_path(book.id)
-    end
+         if book.save
+          redirect_to  book_path(book.id), notice: "Book was successfully created."
+        end
+    end         
 
     def show
         @book = Book.find(params[:id])
@@ -21,21 +22,24 @@ class BooksController < ApplicationController
     def edit
         @book = Book.find(params[:id])
     end
-   
+
     def update
         book = Book.find(params[:id])
-        book.update(book_params)
-        redirect_to book_path(book.id)
+        if book.update(book_params)
+        redirect_to book_path(book.id), notice: "Book was successfully updated."
+        end
     end
 
 
     def  destroy
       books= Book.find(params[:id])
-      books.destroy
-      redirect_to books_path
+      if books.destroy
+      redirect_to books_path, notice: "Book was successfully destroyed."
     end
+    end
+
     private
-     
+
     def book_params
         params.require(:book).permit(:title, :body)
     end
